321173240db670c797d25cd8db5876395c3b0689
krishna <krishnavm143@gmail.com>
2021-11-14 12:46:29 +0530

    done with countdown timer

---
+++ countdown-timer/script.js
@@ -0,0 +1,34 @@
+const newYears = '1 Jan  2022';
+const displayDays=document.getElementById("days-c");
+const displayHours=document.getElementById("hours-c");
+const displayMin=document.getElementById("min-c");
+const displaySec=document.getElementById("sec-c");
+
+function countdown() {
+  const newYearsDate = new Date(newYears)
+  const currentDate = new Date()
+  console.log(newYearsDate - currentDate);
+  const totalSeaconds = (newYearsDate - currentDate) / 1000
+  const days = Math.floor(totalSeaconds / 3600 / 24)
+  //   const hours=Math.floor(days/  24);
+  const hours = Math.floor(totalSeaconds / 3600) % 24;
+  const minutes=Math.floor(totalSeaconds/60)%60;
+  const sec=Math.floor(totalSeaconds)%60;
+//   console.log(totalSeaconds, days, hours,minutes,sec);
+  countChange(days,hours,minutes,sec);    
+//   displayDays.style.innerText=days
+  //   console.log()
+
+}
+// countdown()
+setInterval(countdown,1000)
+
+function countChange(days,hours,minutes,sec){
+    // console.log()
+    // console.log(days)
+    displayDays.innerText=days;
+    displayHours.innerText=hours;
+    displayMin.innerText=minutes;
+    displaySec.innerText=sec;
+
+}
